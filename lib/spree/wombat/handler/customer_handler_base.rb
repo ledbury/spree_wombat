807cf39c26ab284ef558e51da184da66c221a8da
Michael Bianco <iloveitaly@gmail.com>
2016-03-07 19:13:24 -0500

    Search for state by abbr if name match fails

--- lib/spree/wombat/handler/customer_handler_base.rb
+++ lib/spree/wombat/handler/customer_handler_base.rb
@@ -4,7 +4,6 @@ module Spree
       class CustomerHandlerBase < Base
 
         def prepare_address(firstname, lastname, address_attributes)
-
           country_iso = address_attributes.delete(:country)
           country = Spree::Country.find_by_iso(country_iso)
           raise Exception.new("Can't find a country with iso name #{country_iso}!") unless country_iso
@@ -12,7 +11,7 @@ module Spree
 
           state_name = address_attributes.delete(:state)
           if state_name
-            state = Spree::State.find_by_name(state_name)
+            state = Spree::State.find_by_name(state_name) || Spree::State.find_by_abbr(state_name)
             raise Exception.new("Can't find a State with name #{state_name}!") unless state
             address_attributes[:state_id] = state.id
           end
