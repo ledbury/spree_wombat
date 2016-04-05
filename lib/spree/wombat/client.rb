4bb6838f9e841c8a37651fea7b7f2b67a19fec56
Michael Bianco <iloveitaly@gmail.com>
2016-04-05 08:59:54 -0400

    Modify client for single batch pull

--- lib/spree/wombat/client.rb
+++ lib/spree/wombat/client.rb
@@ -6,12 +6,7 @@ module Spree
   module Wombat
     class Client
 
-      def self.push_batches(object, ts_offset = 5)
-        object_count = 0
-
-        last_push_time = Spree::Wombat::Config[:last_pushed_timestamps][object] || Time.at(0)
-        this_push_time = Time.now
-
+      def self.pull(object, last_poll_time)
         payload_builder = Spree::Wombat::Config[:payload_builder][object]
 
         model_name = payload_builder[:model].present? ? payload_builder[:model] : object
@@ -22,52 +17,13 @@ module Spree
           scope = scope.send(filter.to_sym)
         end
 
-        # go 'ts_offset' seconds back in time to catch missing objects
-        last_push_time = last_push_time - ts_offset.seconds
+        serialized_collection = ActiveModel::Serializer::ArraySerializer.new(
+          scope.where(updated_at: last_poll_time...Time.now),
+          serializer: payload_builder[:serializer].constantize,
+          root: payload_builder[:root]
+        )
 
-        scope.where(updated_at: last_push_time...this_push_time).find_in_batches(batch_size: Spree::Wombat::Config[:batch_size]) do |batch|
-          object_count += batch.size
-          serialized_collection = ActiveModel::Serializer::ArraySerializer.new(
-            batch,
-            serializer: payload_builder[:serializer].constantize,
-            root: payload_builder[:root]
-          )
-
-          serializer_adapter = ActiveModel::Serializer::Adapter.create(serialized_collection, adapter: :json)
-
-          push(serializer_adapter.to_json)
-        end
-
-        update_last_pushed(object, this_push_time)
-        object_count
-      end
-
-      def self.push(json_payload)
-        res = HTTParty.post(
-                Spree::Wombat::Config[:push_url],
-                {
-                  body: json_payload,
-                  headers: {
-                   'Content-Type'       => 'application/json',
-                   'X-Hub-Store'        => Spree::Wombat::Config[:connection_id],
-                   'X-Hub-Access-Token' => Spree::Wombat::Config[:connection_token],
-                   'X-Hub-Timestamp'    => Time.now.utc.to_i.to_s
-                  }
-                }
-              )
-
-        validate(res)
-      end
-
-      private
-      def self.update_last_pushed(object, new_last_pushed)
-        last_pushed_ts = Spree::Wombat::Config[:last_pushed_timestamps]
-        last_pushed_ts[object] = new_last_pushed
-        Spree::Wombat::Config[:last_pushed_timestamps] = last_pushed_ts
-      end
-
-      def self.validate(res)
-        raise PushApiError, "Push not successful. Wombat returned response code #{res.code} and message: #{res.body}" if res.code != 202
+        serializer_adapter = ActiveModel::Serializer::Adapter.create(serialized_collection, adapter: :json)
       end
     end
   end
