3f5e97f7655b9bac3eb0c1729a9b881f619fb946
Michael Bianco <iloveitaly@gmail.com>
2016-04-18 12:35:39 -0400

    If address or name information does not exist on the model level, look at last modified order

--- app/serializers/spree/wombat/user_serializer.rb
+++ app/serializers/spree/wombat/user_serializer.rb
@@ -17,6 +17,48 @@ module Spree
       def internal_id
         object.netsuite_customer_id
       end
+
+      # NOTE often the first, last, bill address, and shipping address are not
+      #      set on the user. The logic below pulls these values from the last order
+      #      if they are not on the user model.
+
+      def firstname
+        if (name = object.firstname).present?
+          return name
+        end
+
+        last_order.ship_address.firstname
+      end
+
+      def lastname
+        if (name = object.lastname).present?
+          return name
+        end
+
+        last_order.bill_address.lastname
+      end
+
+      def ship_address
+        if address = object.ship_address
+          return address
+        end
+
+        last_order.ship_address
+      end
+
+      def bill_address
+        if address = object.bill_address
+          return address
+        end
+
+        last_order.bill_address
+      end
+
+      protected
+
+        def last_order
+          @last_order ||= object.orders.complete.last
+        end
     end
   end
 end
